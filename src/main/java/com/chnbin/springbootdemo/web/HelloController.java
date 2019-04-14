fbb9ef752aa1244633c2ba3b7d904e8d2826b49a
chnbin <chnbin@gmail.com>
2019-04-14 15:39:25 +0800

    Recive parameter

--- src/main/java/com/chnbin/springbootdemo/web/HelloController.java
+++ src/main/java/com/chnbin/springbootdemo/web/HelloController.java
@@ -1,6 +1,10 @@
 package com.chnbin.springbootdemo.web;
-import org.springframework.stereotype.Controller;
+import java.util.HashMap;
+import java.util.Map;
+
+// import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,4 +27,18 @@ public class HelloController {
 	public String books() {
 		return "Books";
 	}
+	// 接受參數，GET http://localhost:8080/api/v1/books/{id}
+	// id要對應，如果不要對應的話請用以下寫法
+	// public Object getBookById(@PathVariable("id") long bookId) {
+	@GetMapping("/books/{id}")
+	public Object getBookById(@PathVariable long id) {
+		
+		System.out.println("id: " + id);
+		
+		Map<String, Object> book = new HashMap<>();
+		book.put("name", "Cujo");
+		book.put("author", "Stenphen King");
+		return book;
+	}
+	
 }
